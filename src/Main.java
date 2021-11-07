c7287e622ba6d02db10412eb5513c9332f98f9e3
Monika <monika.bebak@gmail.com>
2021-11-07 18:58:29 +0100

    1234

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* ćw.2 pętle for */
+
+        /* zad.1 Napisz program który będzie iterował od 100 do 0,
+         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        int i = 5;
+        for (; i > 0; ) {
+            System.out.print(i + " ");
+            i--;
+        }
+
+
+        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+         * za pomocą pętli forEach powiększone o 11 */
+
+
+    }
+}
